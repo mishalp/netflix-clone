95d21777627d4a0c9f20282a30244f77982ad070
mishalp <mishalclever@gmail.com>
2022-11-29 13:54:01 +0530

    navbar and banner compleated

---
+++ src/Components/Banner/Banner.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Banner.css'
+
+function Banner() {
+  return (
+    <div className='banner'>
+        <div className="content">
+            <h1 className='title'>Movie Name</h1>
+            <div className="banner-buttons">
+                <button className='button'>Play</button>
+                <button className='button'>My List</button>
+            </div>
+            <h1 className='discription'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document</h1>
+        </div>
+        <div className="fade">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Banner
